ef609bc76571f314b94a2f7a089904e3b40d55e1
Candykiller16 <79221974+Candykiller16@users.noreply.github.com>
2021-03-07 16:02:11 +0300

    Add files via upload

---
+++ task_11_2.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, marka, model, year_of_release, speed = 0 ):
+        self.__marka = marka
+        self.__model = model
+        self.__release = year_of_release
+        self.__speed = speed
+
+    def __increase(self):
+        self.__speed += 10
+        print(f' Ты дал газку и твоя скорость {self.__speed} км/ч')
+
+    def __reduce(self):
+        self.__speed -= 5
+        print(f' Ты сбавил обороты и твоя скорость(против тебя) {self.__speed} км/ч(%)')
+    def __stop(self):
+        self.__speed = 0
+        print(f' Ты тормозишь и твоя скорость {self.__speed} км/ч. Не тормози, сникерс сни')
+    def __show_speed(self):
+        print(f'Ваша скорость {self.__speed} км/ч')
+    def __changes(self):
+        self.__speed *= -1
+        print(f'Ты разворачиваешься и по законам физики твоя скорость {self.__speed} км/ч')
+
+car1 = Car('Tesal', 'modelX', 2012)
+car1._Car__increase()
+car1._Car__reduce()
+car1._Car__stop()
+car1._Car__show_speed()
+car1._Car__changes()
